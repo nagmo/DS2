188484508bd5d875b6298afefe688890b7a7da67
nagmo <nevoagmon@gmail.com>
2017-12-30 21:06:20 +0200

    library - finished

--- library2.cpp
+++ library2.cpp
@@ -1,19 +1,77 @@
+#include <new>
 #include "library2.h"
 #include "Colosseum.h"
 
 void* init(int n, int *trainingGroupsIDs){
+    if(n<2) return NULL;
+    Colosseum* colosseum = NULL;
     try{
-        new Colosseum(n, trainingGroupsIDs);
+        colosseum = new Colosseum(n, trainingGroupsIDs);
+    }catch(std::bad_alloc&){
+        return NULL;
+    }catch(){
+
     }
+    return (void*) colosseum;
 }
 
-StatusType addTrainingGroup(void *DS, int trainingGroupID);
+StatusType addTrainingGroup(void *DS, int trainingGroupID){
+    if(DS == NULL || trainingGroupID < 0) return INVALID_INPUT;
+    try{
+        ((Colosseum*)DS)->addGroup(trainingGroupID);
+    }catch(ColosseumException::InvaldInput&){
+        return INVALID_INPUT;
+    }catch(ColosseumException::Failure&){
+        return FAILURE;
+    }catch(std::bad_alloc&){
+        return ALLOCATION_ERROR;
+    }
+    return SUCCESS;
+}
 
-StatusType addGladiator(void *DS, int gladiatorID, int score, int trainingGroup);
+StatusType addGladiator(void *DS, int gladiatorID,
+                        int score, int trainingGroup){
+    if(DS == NULL || gladiatorID < 0 || score < 0 || trainingGroup < 0)
+        return INVALID_INPUT;
+    try{
+        ((Colosseum*)DS)->addGlad(gladiatorID, score, trainingGroup);
+    }catch(ColosseumException::InvaldInput&){
+        return INVALID_INPUT;
+    }catch(ColosseumException::Failure&){
+        return FAILURE;
+    }catch(std::bad_alloc&){
+        return ALLOCATION_ERROR;
+    }
+    return SUCCESS;
+}
 
-StatusType trainingGroupFight(void *DS, int trainingGroup1, int trainingGroup2, int k1, int k2);
+StatusType trainingGroupFight(void *DS, int trainingGroup1,
+                              int trainingGroup2, int k1, int k2){
+    if(DS == NULL || k1 <= 0 || k2 <= 0 || trainingGroup1 < 0 ||
+            trainingGroup2 <0) return INVALID_INPUT;
+    try{
+        ((Colosseum*)DS)->fight(trainingGroup1, trainingGroup2, k1, k2);
+    }catch(ColosseumException::InvaldInput&){
+        return INVALID_INPUT;
+    }catch(ColosseumException::Failure&){
+        return FAILURE;
+    }catch(std::bad_alloc&){
+        return ALLOCATION_ERROR;
+    }
+    return SUCCESS;
+}
 
-StatusType getMinTrainingGroup(void *DS, int *trainingGroup);
+StatusType getMinTrainingGroup(void *DS, int *trainingGroup){
+    if(DS == NULL || trainingGroup == NULL) return INVALID_INPUT;
+    try{
+        *trainingGroup = ((Colosseum*)DS)->getMinTrainingGroup();
+    }catch(ColosseumException::InvaldInput&){
+        return INVALID_INPUT;
+    }catch(ColosseumException::Failure&){
+        return FAILURE;
+    }
+    return SUCCESS;
+}
 
 /* Description:   Quits and deletes the database.
 *                DS should be set to NULL.
@@ -21,5 +79,9 @@ StatusType getMinTrainingGroup(void *DS, int *trainingGroup);
 * Output:        None.
 * Return Values: None.
 */
-void quit(void** DS);
+void quit(void** DS){
+    if(*DS == NULL) return;
+    delete (*DS);
+    *DS = NULL;
+}
 
